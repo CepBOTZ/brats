93183219a6f29ed765a06942e299b3ccf749f8f9
CepBOTZ <aris.s98@yahoo.com>
2025-01-04 11:17:07 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -15,7 +15,7 @@ let teksnya = `
   • Number : ${m.sender.split("@")[0]}
   • Status : *${isCreator ? "Owner" : isPremium ? "Premium" : "Free User"}*
 
-  *— Othermenu*
+  *— MENU*
   ⳺ .cekidch
   ⳺ .cekidgc
   ⳺ .qc
@@ -23,31 +23,6 @@ let teksnya = `
   ⳺ .readviewonce
   ⳺ .stickerwm
   ⳺ .sticker
-  
-  *— Searchmenu*
-  ⳺ .yts
-  ⳺ .apkmod
-  ⳺ .pinterest
-  
-  *— Toolsmenu*
-  ⳺ .ai
-  ⳺ .gpt
-  ⳺ .tourl
-  ⳺ .tourl2
-  ⳺ .ssweb
-  ⳺ .translate
-  ⳺ .tohd
-  ⳺ .shortlink
-  ⳺ .shortlink2
-  ⳺ .enc
-  
-  *— Shopmenu*
-  ⳺ .buypanel
-  ⳺ .buyadp
-  ⳺ .buyscript
-  ⳺ .buyvps
-  
-  *— Downloadmenu*
   ⳺ .tiktok
   ⳺ .tiktokmp3
   ⳺ .instagram
@@ -57,129 +32,6 @@ let teksnya = `
   ⳺ .playvid
   ⳺ .gitclone
   ⳺ .mediafire
-  
-  *— Storemenu*
-  ⳺ .addrespon
-  ⳺ .delrespon
-  ⳺ .listrespon
-  ⳺ .done
-  ⳺ .proses
-  ⳺ .jpm
-  ⳺ .jpm2
-  ⳺ .jpmtesti
-  ⳺ .jpmslide
-  ⳺ .jpmslideht
-  ⳺ .sendtesti
-  ⳺ .pushkontak
-  ⳺ .pushkontak2
-  ⳺ .payment
-  ⳺ .produk
-  ⳺ .subdomain
-  
-  *— Digitaloceanmenu*
-  ⳺ .r1c1
-  ⳺ .r2c1
-  ⳺ .r4c2
-  ⳺ .r8c4
-  ⳺ .r16c4
-  ⳺ .sisadroplet
-  ⳺ .deldroplet
-  ⳺ .listdroplet
-  ⳺ .rebuild
-  ⳺ .restartvps
-  
-  *— Panelmenu Reseller*
-  ⳺ .addseller
-  ⳺ .delseller
-  ⳺ .listseller
-  ⳺ .1gb
-  ⳺ .2gb
-  ⳺ .3gb
-  ⳺ .4gb
-  ⳺ .5gb
-  ⳺ .6gb
-  ⳺ .7gb
-  ⳺ .8gb
-  ⳺ .9gb
-  ⳺ .10gb
-  ⳺ .unlimited
-  ⳺ .cadmin
-  ⳺ .delpanel
-  ⳺ .deladmin
-  ⳺ .listpanel
-  ⳺ .listadmin
-    
-  *— Panelmenu Owner*
-  ⳺ .1gb-v2
-  ⳺ .2gb-v2
-  ⳺ .3gb-v2
-  ⳺ .4gb-v2
-  ⳺ .5gb-v2
-  ⳺ .6gb-v2
-  ⳺ .7gb-v2
-  ⳺ .8gb-v2
-  ⳺ .9gb-v2
-  ⳺ .10gb-v2
-  ⳺ .unlimited-v2
-  ⳺ .cadmin-v2
-  ⳺ .delpanel-v2
-  ⳺ .deladmin-v2
-  ⳺ .listpanel-v2
-  ⳺ .listadmin-v2
-  
-  *— Installermenu*
-  ⳺ .hackbackpanel
-  ⳺ .installpanel
-  ⳺ .installtemastellar
-  ⳺ .installtemabilling
-  ⳺ .installtemaenigma
-  ⳺ .uninstallpanel
-  ⳺ .uninstalltema
-  
-  *— Groupmenu*
-  ⳺ .add
-  ⳺ .kick
-  ⳺ .close
-  ⳺ .open
-  ⳺ .hidetag
-  ⳺ .kudetagc
-  ⳺ .leave
-  ⳺ .tagall
-  ⳺ .promote
-  ⳺ .demote
-  ⳺ .resetlinkgc
-  ⳺ .on
-  ⳺ .off
-  ⳺ .linkgc
-  
-  *— Ownermenu*
-  ⳺ .autoread
-  ⳺ .autopromosi
-  ⳺ .autoreadsw
-  ⳺ .autotyping
-  ⳺ .addplugins
-  ⳺ .listplugins
-  ⳺ .delplugins
-  ⳺ .getplugins
-  ⳺ .saveplugins
-  ⳺ .addowner
-  ⳺ .listowner
-  ⳺ .delowner
-  ⳺ .self/public
-  ⳺ .setimgmenu
-  ⳺ .setimgfake
-  ⳺ .setppbot
-  ⳺ .clearsession
-  ⳺ .clearchat
-  ⳺ .resetdb
-  ⳺ .restartbot
-  ⳺ .getsc
-  ⳺ .getcase
-  ⳺ .listgc
-  ⳺ .joingc
-  ⳺ .joinch
-  ⳺ .upchannel
-  ⳺ .upchannel2
 `
 await Sky.sendMessage(m.chat, {text: teksnya, mentions: [m.sender, global.owner+"@s.whatsapp.net"]}, {quoted: qtext})
 }
