dc37383b8cd6b24a32a1c8b2506b7d5e46f86c5e
CepBOTZ <aris.s98@yahoo.com>
2025-01-05 16:01:04 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -3,35 +3,35 @@ const os = require('os');
 
 let handler = async (m, { Sky, isCreator, isPremium, qtext, runtime, toIDR }) => {
 let teksnya = `
- *乂 Information - Bot*
-  • Botname : *${global.botname2}*
-  • Version : *${global.versi}*
-  • Mode : *${Sky.public ? "Public": "Self"}*
-  • Creator : @${global.owner}
-  • Runtime Bot : *${runtime(process.uptime())}*
-  • Uptime Vps : *${runtime(os.uptime())}*
-  
- *乂 Information - Users*
-  • Number : ${m.sender.split("@")[0]}
-  • Status : *${isCreator ? "Owner" : isPremium ? "Premium" : "Free User"}*
-
-  *— MENU*
-  ⳺ .cekidch
-  ⳺ .cekidgc
-  ⳺ .qc
-  ⳺ .brat
-  ⳺ .readviewonce
-  ⳺ .stickerwm
-  ⳺ .sticker
-  ⳺ .tiktok
-  ⳺ .tiktokmp3
-  ⳺ .instagram
-  ⳺ .ytmp3
-  ⳺ .ytmp4
-  ⳺ .play
-  ⳺ .playvid
-  ⳺ .gitclone
-  ⳺ .mediafire
+> *乂 Information - Bot 乂*
+`⭑ Botname :` *${global.botname2}*
+`⭑ Version :` *${global.versi}*
+`⭑ Mode :` *${Sky.public ? "Public": "Self"}*
+`⭑ Creator :` @⁨${global.owner} *✰*
+`⭑ Runtime Bot :` *${runtime(process.uptime())}*
+`⭑ Uptime Vps :` *${runtime(os.uptime())}*
+> *乂 Information - Users 乂*
+`⭑ Number :` ${m.sender.split("@")[0]}
+`⭑ Status :` *${isCreator ? "Owner" : isPremium ? "Premium" : "Free User"}*
+> *— STICKER MENU —*
+  ➤ `.qc`
+  ➤ `.brat`
+  ➤ `.sticker`
+  ➤ `.stickerwm`
+> *— DOWNLOAD MENU —*
+  ➤ `.tiktok`
+  ➤ `.tiktokmp3`
+  ➤ `.instagram`
+  ➤ `.ytmp3`
+  ➤ `.ytmp4`
+  ➤ `.play`
+  ➤ `.playvid`
+  ➤ `.gitclone`
+  ➤ `.mediafire`
+> *— OTHER —*
+  ➤ `.cekidch`
+  ➤ `.cekidgc`
+  ➤ `.readviewonce`
 `
 await Sky.sendMessage(m.chat, {text: teksnya, mentions: [m.sender, global.owner+"@s.whatsapp.net"]}, {quoted: qtext})
 }
